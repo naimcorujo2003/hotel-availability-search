4ffbb98dd3ed239370ee1a6bc02908fa948b2b50
Naim Corujo <naimcorujo.work@gmail.com>
2026-04-14 21:18:17 -0300

    chore: initialize project and scaffold hexagonal architecture structure

---
+++ hotelsearch/src/main/java/com/riu/hotels/hotel_availability_search/HotelAvailabilitySearchApplication.java
@@ -0,0 +1,13 @@
+package com.riu.hotels.hotel_availability_search;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelAvailabilitySearchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotelAvailabilitySearchApplication.class, args);
+	}
+
+}
---
+++ hotelsearch/src/test/java/com/riu/hotels/hotel_availability_search/HotelAvailabilitySearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.riu.hotels.hotel_availability_search;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelAvailabilitySearchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
